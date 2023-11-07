228e31af380fae4b562402b0456f36f10081276d
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2023-11-07 14:15:10 +0100

    ultimo

--- app/src/main/java/com/example/di_ejerciciocard/MainActivity.kt
+++ app/src/main/java/com/example/di_ejerciciocard/MainActivity.kt
@@ -3,36 +3,26 @@ package com.example.di_ejerciciocard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.di_ejerciciocard.ui.theme.Di_ejerciciocardTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,13 +35,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    EjercicioCard()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showSystemUi = true)
 @Composable
 fun EjercicioCard() {
@@ -64,50 +55,122 @@ fun EjercicioCard() {
                 .padding(20.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
         ) {
+            var prioridadBadge by rememberSaveable {
+                mutableStateOf("baja")
+            }
             Column(verticalArrangement = Arrangement.SpaceBetween) {
-
-
-                Box(modifier = Modifier.weight(1f)) {
-                    Text(text = "Tarea 1")
-                }
-                Box(modifier = Modifier.weight(1f)) {
-                    var prioridad by rememberSaveable {
-                        mutableStateOf("baja")
-                    }
-                    @OptIn(ExperimentalMaterial3Api::class)
-                    BadgedBox(badge = { Badge { Text(prioridad) } }) {
-                        Icon(
-                            imageVector = Icons.Default.Build, contentDescription = null,
-                            modifier = Modifier.size(40.dp)
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                        Text(
+                            text = "Tarea 1",
+                            modifier =
+                            Modifier
+                                .padding(15.dp)
+                                .weight(3f),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            textAlign = TextAlign.Center
                         )
+                        Box(
+                            modifier =
+                            Modifier
+                                .padding(15.dp)
+                                .weight(1f)
+                        ) {
+                            @OptIn(ExperimentalMaterial3Api::class)
+                            BadgedBox(badge = { Badge { Text(prioridadBadge) } }) {
+                                Icon(
+                                    imageVector = Icons.Default.Build, contentDescription = null,
+                                    modifier = Modifier.size(40.dp)
+                                )
+                            }
+                        }
                     }
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(text = "prioridad")
-                }
-                var expandedEstado by rememberSaveable {
-                    mutableStateOf(false)
-                }
-                val estado = listOf("Completada", "No completada")
-                Box(modifier = Modifier.weight(1f)) {
-                    DropdownMenu(
-                        expanded = expandedEstado, onDismissRequest = { expandedEstado = false },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
 
+                    Text(
+                        text = "Realizar pruebas", modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(15.dp),
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Justify
+                    )
+                }
+
+
+                Box(modifier = Modifier.weight(1f)) {
+                    var selectedText by rememberSaveable {
+                        mutableStateOf("..")
+                    }
+                    var expanded by rememberSaveable {
+                        mutableStateOf(false)
+                    }
+
+                    val prioridades = listOf("Alta", "Media", "Baja")
+                    Column(modifier = Modifier.padding(20.dp)) {
+                        OutlinedTextField(
+                            value = selectedText,
+                            onValueChange = { selectedText = it },
+                            enabled = false,
+                            readOnly = true,
+                            modifier = Modifier
+                                .clickable { expanded = true }
+                                .fillMaxWidth()
+                        )
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            prioridades.forEach { prioridad ->
+                                DropdownMenuItem(
+                                    text = { Text(text = prioridad) },
+                                    onClick = {
+                                        expanded = false
+                                        selectedText = prioridad
+                                    }
+                                )
+                            }
+                            prioridadBadge = selectedText
+                        }
                     }
                 }
-
-                var expandedPrioridad by rememberSaveable {
-                    mutableStateOf(false)
-                }
-                val prioridad = listOf("Completada", "No completada")
                 Box(modifier = Modifier.weight(1f)) {
-                    DropdownMenu(
-                        expanded = expandedPrioridad,
-                        onDismissRequest = { expandedPrioridad = false },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
+                    var selectedText by rememberSaveable {
+                        mutableStateOf("..")
+                    }
+                    var expanded by rememberSaveable {
+                        mutableStateOf(false)
+                    }
 
+                    val estados = listOf("Completada", "No completada")
+                    Column(modifier = Modifier.padding(20.dp)) {
+                        OutlinedTextField(
+                            value = selectedText,
+                            onValueChange = { selectedText = it },
+                            enabled = false,
+                            readOnly = true,
+                            modifier = Modifier
+                                .clickable { expanded = true }
+                                .fillMaxWidth()
+                        )
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            estados.forEach { estado ->
+                                DropdownMenuItem(
+                                    text = { Text(text = estado) },
+                                    onClick = {
+                                        expanded = false
+                                        selectedText = estado
+                                    }
+                                )
+                            }
+                        }
                     }
                 }
             }
